3b795b3d3ad6a2ccc87106bf42423f44da30a7a5
spaos11 <elekpato11@gmail.com>
2019-03-23 21:40:54 +0800

    taetae

---
+++ www/new/distinct.php
@@ -0,0 +1,9 @@
+<?php
+
+                    $query = "SELECT DISTINCT(product_brand) FROM product WHERE product_status = '1' ORDER BY product_id DESC";
+                    $statement = $connect->prepare($query);
+                    $statement->execute();
+                    $result = $statement->fetchAll();
+                    foreach($result as $row)
+                    {
+                    ?>
