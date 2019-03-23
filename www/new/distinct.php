635490d6b6cf25d6bf0440bf6801985e360d3239
spaos11 <elekpato11@gmail.com>
2019-03-23 22:02:48 +0800

    deleted

--- www/new/distinct.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-                    $query = "SELECT DISTINCT(product_brand) FROM product WHERE product_status = '1' ORDER BY product_id DESC";
-                    $statement = $connect->prepare($query);
-                    $statement->execute();
-                    $result = $statement->fetchAll();
-                    foreach($result as $row)
-                    {
-                    ?>
